ca13adcfe8682994071e38103c6979fa316cf2b0
vvc12 <velchuri8885405535@gmail.com>
2024-06-30 17:55:00 +0530

    Complete Pandas

---
+++ pandas/new.ipnyb.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+get_ipython().run_line_magic('pinfo', '%%time')
+get_ipython().run_cell_magic('time', 'for(i in range(10000)) : i*i', '')
+get_ipython().run_cell_magic('time', 'for i in range(10000): i*i', '')
+for i in range (10000):i*i
+for i in range (10000):i*i
+    get_ipython().run_line_magic('%time', '')
+for i in range (10000):i*i
+get_ipython().run_line_magic('pinfo', '%%time')
+get_ipython().run_cell_magic('time', 'for i in range (10000):i*i', '')
+get_ipython().run_line_magic('time', 'for i in range (10000):i*i')
+get_ipython().run_line_magic('time', '2**1234556')
+get_ipython().run_cell_magic('!?', '', '')
+get_ipython().system('?')
+get_ipython().run_line_magic('', '! ?')
+get_ipython().run_line_magic('!?', '')
+get_ipython().run_cell_magic('!?', '', '')
+get_ipython().run_line_magic('pinfo', '%%HTML')
+get_ipython().run_line_magic('pinfo', '%HTML')
+get_ipython().run_line_magic('pinfo', '%time')
+get_ipython().run_cell_magic('time', '', 'for i in range(100000):\n    i*i\n')
+get_ipython().run_line_magic('system', 'pwd')
+get_ipython().run_cell_magic('bash', 'pwd', '')
+get_ipython().run_line_magic('bash', 'pwd')
+get_ipython().run_cell_magic('bash', '', 'pwd\n')
+get_ipython().run_line_magic('pinfo', '%bash')
+get_ipython().run_line_magic('pinfo', '%%bash')
+import os
+os.getcwd()
+get_ipython().getoutput('dir')
+get_ipython().run_line_magic('pwd', '')
+<iframe width="560" height="315" src="https://www.youtube.com/embed/EEEZX_0FMEc?si=smYour-PbWfquYnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+get_ipython().run_cell_magic('HTML', '', '<iframe width="560" height="315" src="https://www.youtube.com/embed/EEEZX_0FMEc?si=smYour-PbWfquYnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>\n')
+get_ipython().run_line_magic('pinfo', '%%md')
+get_ipython().run_line_magic('pinfo', '%%markdown')
+get_ipython().run_line_magic('preview', 'Markdown Cheat Sheet _ Markdown Guide.pdf')
+get_ipython().run_cell_magic('preview', 'Markdown Cheat Sheet _ Markdown Guide.pdf', '')
+pdf_path = "Markdown Cheat Sheet _ Markdown Guide.pdf"
+
+embed_code = f'<embed src="{pdf_path}" type="application/pdf" width="100%" height="600px"/>'  
+get_ipython().run_cell_magic('HTML', '', 'pdf_path = "Markdown Cheat Sheet _ Markdown Guide.pdf"\n\nembed_code = f\'<embed src="{pdf_path}" type="application/pdf" width="100%" height="600px"/>\'  \n')
+get_ipython().run_cell_magic('HTML', '', 'pdf_path = "Markdown Cheat Sheet _ Markdown Guide.pdf"\n<a href={pdf_path} target="_blank">Link Text</a>\n')
+get_ipython().run_cell_magic('HTML', '', "<style>\n    /* Example of using different fonts */\n    h1 {\n        font-family: 'Arial', sans-serif;\n    }\n    p {\n        font-family: 'Times New Roman', serif;\n    }\n    .custom-font {\n        font-family: 'Courier New', monospace;\n    }\n</style>\n")
+<style>
+    *(Example, of, using, different, fonts, */)
+    h1 {
+        font-family: 'Arial', sans-serif;
+    }
+    p {
+        font-family: 'Times New Roman', serif;
+    }
+    .custom-font {
+        font-family: 'Courier New', monospace;
+    }
+</style>
+get_ipython().run_cell_magic('HTMKL', '', "<style>\n    /* Example of using different fonts */\n    h1 {\n        font-family: 'Arial', sans-serif;\n    }\n    p {\n        font-family: 'Times New Roman', serif;\n    }\n    .custom-font {\n        font-family: 'Courier New', monospace;\n    }\n</style>\n")
+get_ipython().run_cell_magic('HTML', '', '<iframe width="560" height="315" src="https://www.youtube.com/embed/EEEZX_0FMEc?si=smYour-PbWfquYnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>\n')
+get_ipython().run_cell_magic('HTML', '', "<style>\n    /* Example of using different fonts */\n    h1 {\n        font-family: 'Arial', sans-serif;\n    }\n    p {\n        font-family: 'Times New Roman', serif;\n    }\n    .custom-font {\n        font-family: 'Courier New', monospace;\n    }\n</style>\n")
+<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=400px,height=100px ">
+<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=400px;height=100px;" >
+<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" width=400px height=100px >
+get_ipython().system('[Markdown Cheat Sheet](https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png)')
+get_ipython().system('(https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png)')
+get_ipython().system('[markdown_cheat_sheet.png](https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png)')
+get_ipython().run_cell_magic('HTML', '', '<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=400px;height=100px;" >\n')
+get_ipython().run_cell_magic('HTML', '', '<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=400px;height=120px;" >\n')
+get_ipython().run_cell_magic('HTML', '', '<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=500pxpx;height=120px;" >\n')
+get_ipython().run_cell_magic('HTML', '', '<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=500px;height=120px;" >\n')
+get_ipython().run_cell_magic('HTML', '', '<img src="https://images.datacamp.com/image/upload/v1697798108/Markdown_Cheat_Sheet_9657d9746f.png" style="width=500px;height=120px;" >\n')
+get_ipython().run_line_magic('pinfo', '%%code_wrap')
+get_ipython().run_line_magic('pinfo', '%%SVG')
+get_ipython().run_line_magic('pinfo', '%%svg')
+get_ipython().run_line_magic('pinfo', '%%capture')
+get_ipython().run_line_magic('pinfo', '%%ddir')
+get_ipython().run_line_magic('pinfo', '%%ddir')
+get_ipython().run_cell_magic('ddir', '', '')
+get_ipython().run_line_magic('ddir', '')
+get_ipython().run_line_magic('save', '')
